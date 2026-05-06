6735e51400ba7b7259d323e92e24a0bd90cc1a4b
VladislavKovalenko <soros765@gmail.com>
2026-05-06 15:34:58 +0300

    Editor Tool for scripts search
    - New Tool ScriptUsageFinder
    - New Menu Buttons

--- Assets/1GameProject/Scripts/GameManagement/ADTest.cs
+++ Assets/1GameProject/Scripts/GameManagement/ADTest.cs
@@ -1,9 +1,13 @@
-﻿// using YG;
+﻿using UnityEngine;
+using YG;
 
 namespace _1GameProject.Scripts.GameManagement
 {
-    public class ADTest
+    public class ADTest : MonoBehaviour
     {
-        
+        void Start()
+        {
+            YG2.SaveProgress();
+        }
     }
 }
